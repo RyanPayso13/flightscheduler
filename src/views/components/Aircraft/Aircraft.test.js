20533e791968e275b262c4a30584e60b678a643d
RyanPayso13 <ryan.pays@gmail.com>
2019-06-25 18:14:08 +0100

    Aircraft integration test

---
+++ src/views/components/Aircraft/Aircraft.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Aircraft from './Aircraft';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+function setup() {
+    const props = {
+        ident: 'GABCD',
+        type: 'A320',
+        base: 'EGKK'
+    };
+    const enzymeWrapper = shallow(<Aircraft {...props} />)
+
+    return {
+        props,
+        enzymeWrapper
+    };
+}
+
+describe('Aircraft Component', () => {
+
+    it('should render self', () => {
+        const { enzymeWrapper } = setup()
+        const aircraftInfo = enzymeWrapper.find('p');
+
+        expect(aircraftInfo.length).toEqual(3);
+        expect(aircraftInfo.at(0).text()).toEqual('GABCD');
+        expect(aircraftInfo.at(1).text()).toEqual('A320');
+        expect(aircraftInfo.at(2).text()).toEqual('EGKK');
+      });
+
+});
