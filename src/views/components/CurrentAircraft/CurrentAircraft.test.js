e21cf61e77f39b0bd6583f654c775dadeb1f9782
RyanPayso13 <ryan.pays@gmail.com>
2019-07-01 00:02:41 +0100

    update test code for current aircraft

--- src/views/components/CurrentAircraft/CurrentAircraft.test.js
+++ src/views/components/CurrentAircraft/CurrentAircraft.test.js
@@ -2,13 +2,35 @@ import React from 'react';
 import {
     render  
 } from '@testing-library/react';
+import Context from '../../../state/context';
 import CurrentAircraft from './CurrentAircraft';
 
-describe('<CurrentAircraft />', () => {
+function generateContextTree(state = {}) {
+    return (
+        <Context.Provider value={{ state }}>
+          <CurrentAircraft />
+        </Context.Provider>
+      );
+}
 
-    it('should render self', () => {
-        const { getByText } = render(<CurrentAircraft />);
-        expect(getByText('Current Aircraft: FOOBAR!')).toHaveTextContent('Current Aircraft: FOOBAR!');
+fdescribe('<CurrentAircraft />', () => {
+
+    it('should render the initial state', () => {
+        const state = {
+            currentAircraft: ''
+        };
+        const tree = generateContextTree(state);
+        const { getByText } = render(tree);
+        expect(getByText('Current Aircraft:')).toHaveTextContent('Current Aircraft:');
+    });
+
+    it('should render an aircraft id', () => {
+        const state = {
+            currentAircraft: 'A380'
+        };
+        const tree = generateContextTree(state);
+        const { getByText } = render(tree);
+        expect(getByText('Current Aircraft: A380')).toHaveTextContent('Current Aircraft:');
     });
 
 });
