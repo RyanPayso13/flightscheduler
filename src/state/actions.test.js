06293c00e1e87641dc4ca01f99c6c9daa6de788e
RyanPayso13 <ryan.pays@gmail.com>
2019-07-01 13:11:52 +0100

    action unit tests

---
+++ src/state/actions.test.js
@@ -0,0 +1,32 @@
+import * as ACTION_TYPES from './constants';
+import * as actions from './actions';
+
+describe('Actions', () => {
+
+    it('should set the current aircraft', () => {
+        const payload = 'GABCD';
+        const expectedAction = {
+            type: ACTION_TYPES.SET_CURRENT_AIRCRAFT,
+            payload
+        };
+        expect(actions.setCurrentAircraft(payload)).toEqual(expectedAction);
+    });
+
+    it('should schedule a flight', () => {
+        const payload = {
+            "id":"AS1001",
+            "departuretime":21600,
+            "arrivaltime":26100,
+            "readable_departure":"06:00",
+            "readable_arrival":"07:15",
+            "origin":"LFSB",
+            "destination":"LFMN"
+        };
+        const expectedAction = {
+            type: ACTION_TYPES.ADD_FLIGHT_TO_SCHEDULE,
+            payload
+        };
+        expect(actions.addFlightToSchedue(payload)).toEqual(expectedAction);
+    });
+
+});
