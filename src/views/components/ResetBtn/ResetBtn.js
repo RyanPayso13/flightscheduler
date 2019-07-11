3509fa8cc88f757f037b1ea42b5f02cdd0f194d8
RyanPayso13 <ryan.pays@gmail.com>
2019-07-11 19:24:07 +0100

    add disabled btn bg styling

--- src/views/components/ResetBtn/ResetBtn.js
+++ src/views/components/ResetBtn/ResetBtn.js
@@ -6,19 +6,22 @@ const ResetBtn = () => {
 
     const { state, dispatch } = useContext(Context);
     const [ disabled, setDisabled ] = useState('');
+    const [ btnClass, setBtnClass ] = useState('');
     const handleClick = () => {
         dispatch(resetSchedule());
     };
+    let classes = `${ btnClass } self-end text-white font-bold py-2 px-4 rounded`;
 
     useEffect(() => {
         setDisabled(state.scheduledFlights.length === 0 ? 'disabled' : '');
+        setBtnClass(state.scheduledFlights.length === 0 ? 'bg-gray-500 cursor-default' : 'bg-blue-500 hover:bg-blue-600');
     }, [state.scheduledFlights]);
 
     return (
         <button
             data-testid="reset-btn"
             type="button"
-            className="self-end bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" 
+            className={ classes } 
             onClick={ handleClick }
             disabled={ disabled }>
                 Reset Schedule
